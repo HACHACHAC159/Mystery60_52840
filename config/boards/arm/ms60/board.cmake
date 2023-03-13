967cb258d7f6e1d2ca1c432564f15f67068b7ae5
HACHACHAC159 <47084688+HACHACHAC159@users.noreply.github.com>
2023-03-13 23:54:20 +0700

    Update board.cmake

--- config/boards/arm/ms60/board.cmake
+++ config/boards/arm/ms60/board.cmake
@@ -1,7 +1,2 @@
-# Copyright (c) 2021 Nick Winans
-# SPDX-License-Identifier: MIT
-
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+board_runner_args(pyocd "--target=nrf52832")
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
